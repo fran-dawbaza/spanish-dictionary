50c98a7a48e04d80ef2eecd7f045fe8affa2c6f4
fran-dawbaza <71774925+fran-dawbaza@users.noreply.github.com>
2022-12-07 09:27:50 +0100

    Update leerDiccionarioConScriptSrc.js

--- leerDiccionarioConScriptSrc.js
+++ leerDiccionarioConScriptSrc.js
@@ -1,2 +1,5 @@
-// Previamente se cargó el diccionario con  <script src="https://cdn.jsdelivr.net/gh/fran-dawbaza/spanish-dictionary/diccionarioSinExport.js"></script>
+// Previamente se cargó el diccionario desde la etiqueta script en el archivo html:
+// <script src="https://cdn.jsdelivr.net/gh/fran-dawbaza/spanish-dictionary/diccionarioSinExport.js"></script>
+// La ruta https://cdn.jsdelivr.net/gh/fran-dawbaza/spanish-dictionary/diccionarioSinExport.js hace referencia
+// al archivo
 console.log(diccionario);
