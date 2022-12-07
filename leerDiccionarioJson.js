3efec501e500e7052d7284733ce0b8f79d06597e
fran-dawbaza <71774925+fran-dawbaza@users.noreply.github.com>
2022-12-07 09:39:41 +0100

    Update leerDiccionarioJson.js

--- leerDiccionarioJson.js
+++ leerDiccionarioJson.js
@@ -1,13 +1,25 @@
 
 
-// Dentro de la función tenemos acceso a diccionario
-async function leeDiccionario(){
+// Dentro de la función main tenemos acceso al array diccionario, puedes hacer tu aplicación dentro de esta función o usar un callback
+async function leeDiccionario(callBack){
+    // También podemos usar la ruta https://cdn.jsdelivr.net/gh/fran-dawbaza/spanish-dictionary/diccionario.json
     const respuesta = await fetch('https://raw.githubusercontent.com/fran-dawbaza/spanish-dictionary/main/diccionario.json');
     const diccionario = await respuesta.json();
-    console.log(diccionario);
-    console.log("¿patata está en el diccionario? ", diccionario.includes('patata'));
+    // console.log(diccionario);
+    // console.log("¿patata está en el diccionario? ", diccionario.includes('patata'));
+    if (callBack)
+        callBack(diccionario);
 }
 
 leeDiccionario();
 
+/* 
+o bien
 
+function miPrograma(diccionario){
+  //...
+}
+
+leerDiccionario(miPrograma);
+
+*/
