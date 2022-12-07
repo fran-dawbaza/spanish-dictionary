55c5d3ae1daa20912151efb67d7e6037224eb9f9
fran-dawbaza <71774925+fran-dawbaza@users.noreply.github.com>
2022-12-07 09:31:40 +0100

    Update leerDiccionarioConModulos.js

--- leerDiccionarioConModulos.js
+++ leerDiccionarioConModulos.js
@@ -1,3 +1,6 @@
+// Es necesario usar type="module" en la carga de este script para usar módulos ES
+// La ruta https://cdn.jsdelivr.net/gh/fran-dawbaza/spanish-dictionary/diccionario.js hace referencia
+// al archivo https://github.com/fran-dawbaza/spanish-dictionary/diccionario.js
 import {diccionario} from "https://cdn.jsdelivr.net/gh/fran-dawbaza/spanish-dictionary/diccionario.js";
 
 console.log(diccionario);
